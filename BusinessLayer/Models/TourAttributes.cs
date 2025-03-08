fdb78701b4964a770b3634b9af6fa5ed34143683
salmam01 <if23b281@technikum-wien.at>
2025-03-08 19:07:17 +0100

    Updated View

---
+++ BusinessLayer/Models/TourAttributes.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TourPlanner.BusinessLayer.Models
+{
+    public class TourAttributes
+    {
+        public double Popularity { get; set; }
+        public bool ChildFriendliness { get; set; }
+        public double SearchRanking { get; set; } // not sure what to name it yet
+    }
+}
