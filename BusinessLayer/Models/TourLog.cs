f624aac7e4198f2ddb20079920f73a53e5320e69
Zeliha Vural <zeliha@outlook.com>
2025-03-23 15:08:46 +0100

    tourlog end

--- BusinessLayer/Models/TourLog.cs
+++ BusinessLayer/Models/TourLog.cs
@@ -6,8 +6,7 @@ using System.Threading.Tasks;
 
 namespace TourPlanner.BusinessLayer.Models
 {
-    public class TourLog
-    {
+    public class TourLog {
         public Guid Id { get; set; }
         public DateTime Date { get; set; }
         public int Difficulty { get; set; }
@@ -15,5 +14,27 @@ namespace TourPlanner.BusinessLayer.Models
         public string Comment { get; set; }
         public double TotalDistance { get; set; }
         public TimeSpan TotalTime { get; set; }
+
+
+        public TourLog(DateTime date, int difficulty, double rating, string comment, double totalDistance, TimeSpan totalTime) {
+            Id = Guid.NewGuid();
+            Date = date;
+            Difficulty = difficulty;
+            Rating = rating;
+            Comment = comment;
+            TotalDistance = totalDistance;
+            TotalTime = totalTime;
+        }
+        public TourLog() {
+            Id = Guid.NewGuid();
+            Date = DateTime.Now;
+            Difficulty = 1;
+            Rating = 1;
+            Comment = string.Empty;
+            TotalDistance = 0;
+            TotalTime = TimeSpan.Zero;
+        }
+
     }
+
 }
