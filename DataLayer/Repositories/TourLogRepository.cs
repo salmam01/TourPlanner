a65ef098a9de58fb779a2e18b1bcd0d2809b5b01
salmam01 <if23b281@technikum-wien.at>
2025-05-03 15:34:42 +0200

    Added Repository

--- DataLayer/Repositories/TourLogRepository.cs
+++ DataLayer/Repositories/TourLogRepository.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace TourPlanner.DataLayer.Repositories
 {
-    class TourLogRepository
+    public class TourLogRepository
     {
     }
 }
--- DataLayer/Repositories/TourRepository.cs
+++ DataLayer/Repositories/TourRepository.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace TourPlanner.DataLayer.Repositories
 {
-    class TourRepository
+    public class TourRepository
     {
     }
 }
