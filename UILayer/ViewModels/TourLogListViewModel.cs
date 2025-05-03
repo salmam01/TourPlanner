c378a01601c86c55ac03197bd5885313d409c789
salmam01 <if23b281@technikum-wien.at>
2025-05-03 15:30:15 +0200

    Upgraded Project to .NET Version from 4.8 to 8.0

--- UILayer/ViewModels/TourLogListViewModel.cs
+++ UILayer/ViewModels/TourLogListViewModel.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
-using System.Runtime.Remoting.Messaging;
 using System.Text;
 using System.Threading.Tasks;
 using TourPlanner.BusinessLayer.Models;
