d39e071907b4f21ea42e4b0133189017d755d047
salmam01 <if23b281@technikum-wien.at>
2025-06-01 15:56:08 +0200

    Added Logging to TourService

--- TourPlanner.BL/Services/TourService.cs
+++ TourPlanner.BL/Services/TourService.cs
@@ -1,6 +1,7 @@
 ﻿using TourPlanner.Models.Entities;
 using TourPlanner.DAL.Repositories.TourRepository;
 using Microsoft.Extensions.Logging;
+using Npgsql;
 
 namespace TourPlanner.BL.Services;
 
@@ -24,11 +25,47 @@ public class TourService {
     }
     
     public void CreateTour(Tour tour) {
-        _tourRepository.InsertTour(tour);
+        if (tour == null)
+        {
+            _logger.LogWarning("Trying to create Tour with NULL Tour.");
+            return;
+        }
+
+        try
+        {
+            _tourRepository.InsertTour(tour);
+            _logger.LogInformation("Tour created => {@Tour}", tour);
+        }
+        catch (PostgresException pgEx)
+        {
+            _logger.LogError(pgEx, "Postgres Exception occurred while creating Tour => {TourName}: {ErrorCode} -> {Message}", tour.Name, pgEx.SqlState, pgEx.MessageText);
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "Exception occurred while creating Tour => {TourName}", tour.Name);
+        }
     }
     
     public void UpdateTour(Tour tour) {
-        _tourRepository.UpdateTour(tour);
+        if (tour == null)
+        {
+            _logger.LogWarning("Trying to update Tour with NULL Tour.");
+            return;
+        }
+
+        try
+        {
+            _tourRepository.UpdateTour(tour);
+            _logger.LogInformation("Tour updated => {Tour}", tour);
+        }
+        catch (PostgresException pgEx)
+        {
+            _logger.LogError(pgEx, "Postgres Exception occurred while updating Tour => {TourName}: {ErrorCode} -> {Message}", tour.Name, pgEx.SqlState, pgEx.MessageText);
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "Exception occurred while updating Tour => {TourName}", tour.Name);
+        }
     }
 
     public IEnumerable<Tour> SearchTours(string query) {
@@ -37,12 +74,47 @@ public class TourService {
 
     public void RecalculateTourAttributes(Tour tour)
     {
-        _tourAttributesService.UpdateAttributes(tour);
+        if (tour == null)
+        {
+            _logger.LogWarning("Trying to recalculate Tour Attributes with NULL Tour.");
+            return;
+        }
+        
+        try
+        {
+            _tourAttributesService.UpdateAttributes(tour);
+            _logger.LogInformation("Tour Attributes recalculated => {TourName}: {@TourAttributes}", tour.Name, tour.TourAttributes);
+
+        }
+        catch (PostgresException pgEx)
+        {
+            _logger.LogError(pgEx, "Postgres Exception occurred while recalculating Tour Attributes => {TourName}: {ErrorCode} -> {Message}", tour.Name, pgEx.SqlState, pgEx.MessageText);
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "Exception occurred while recalculating Tour Attributes => {TourName}", tour.Name);
+        }
     }
 
     public void DeleteTour(Tour tour) {
-        if (tour != null) {
+        if (tour == null) 
+        {
+            _logger.LogWarning("Trying to delete Tour with NULL Tour.");
+            return;
+        }
+
+        try
+        {
+            _logger.LogInformation("Tour deleted => {@TourName}", tour.Name);
             _tourRepository.DeleteTour(tour.Id);
+        }
+        catch (PostgresException pgEx)
+        {
+            _logger.LogError(pgEx, "Postgres Exception occurred while deleting Tour => {TourName}: {ErrorCode} -> {Message}", tour.Name, pgEx.SqlState, pgEx.MessageText);
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "Exception occurred while deleting Tour => {TourName}", tour.Name);
         }
     }
 }
